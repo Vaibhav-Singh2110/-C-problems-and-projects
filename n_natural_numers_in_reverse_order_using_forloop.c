8ae4796d93cde9c587e3b34c0a3d15ad5925fa18
Vaibhav-Singh2110 <77459231+Vaibhav-Singh2110@users.noreply.github.com>
2021-05-20 23:23:12 +0530

    Add files via upload

---
+++ n_natural_numers_in_reverse_order_using_forloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+
+int main(){
+     printf("The first 100 natural numbers in reverse order are as follows: \n");
+     for(int i = 10; i; i--){
+         printf("%d \n", i);
+     }
+     return 0;
+}
